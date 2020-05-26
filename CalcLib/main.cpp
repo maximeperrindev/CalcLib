2d82987fb7865ffd4a594280b4b31001f4bccfbc
Maxime Perrin <maxime.perrin@telecom-st-etienne.fr>
2020-05-26 12:32:17 +0200

    Création du repo

---
+++ CalcLib/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CalcLib
+//
+//  Created by Maxime Perrin on 26/05/2020.
+//  Copyright © 2020 Maxime Perrin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
