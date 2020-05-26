b62d9ddc2bc6b741679acc11d507b6c0412c42af
valmontchovet <val.montchovet@hotmail.fr>
2020-05-26 15:09:14 +0200

    Algorithme addition statique

--- CalcLib/main.cpp
+++ CalcLib/main.cpp
@@ -8,8 +8,56 @@
 
 #include <iostream>
 
+using namespace std;
+
+typedef struct {
+    unsigned int* p;
+    unsigned int size;
+}
+lentier;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    lentier a;
+    lentier b;
+    lentier s;
+
+    a.size = 10;
+    a.p = new unsigned int[a.size](); 
+
+    b.size = 10;
+    b.p = new unsigned int[b.size];
+
+    for (int i = 0; i < 10; i++) {
+        a.p[i] = 9;
+        b.p[i] = 9;
+        cout << a.p[i];
+    }
+    cout << endl;
+    s.size = 11;
+    s.p = new unsigned int[s.size];
+
+    unsigned int c = 0;
+    for (int i = s.size-2; i >= 0; i--) {
+
+        s.p[i+1] = (a.p[i] + b.p[i] + c) % 10;
+
+        if (a.p[i] + b.p[i] + c < 10) {
+
+            c = 0;
+        }
+        else
+        {
+            c = 1;
+        }
+    }
+    s.p[0] = c;
+    for (int i = 0; i < 11; i++) {
+        cout << s.p[i];
+    }
+
+    delete[] a.p;
+    delete[] b.p;
+    delete[] s.p;
     return 0;
 }
