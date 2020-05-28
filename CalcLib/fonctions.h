98e91eb56772e53fb6752610d6b0c0c76aed8fa5
Yvan Enjolras <yvannavy42@gmail.com>
2020-05-28 14:23:33 +0200

    Fonctions.h et fonctions.cpp avec main allégé

---
+++ CalcLib/fonctions.h
@@ -0,0 +1,17 @@
+
+
+typedef struct {
+	unsigned int* p;
+	unsigned int size;
+}
+lentier;
+
+int getNumbersChar(char* );
+
+int tailleBase(int, char *);
+
+void checkCharLength(char*, char*);
+
+void remplirTableau(int, int, char* , unsigned int* );
+
+void dynamicChar(char*&);
--- CalcLib/main.cpp
+++ CalcLib/main.cpp
@@ -8,97 +8,10 @@
 
 #include <iostream>
 #include <math.h>
-
 using namespace std;
 
-typedef struct {
-    unsigned int* p;
-    unsigned int size;
-}
-lentier;
+#include "fonctions.h"
 
-int getNumbersChar(char* a) {
-    return strlen(a);
-}
-
-int tailleBase(int taille, char *a) {
-    unsigned long int nombre = 0;
-    int indice = 0;
-    int nombreCase = 0;
-    int indicePuissance = 0;
-    while (indice<taille) {
-        nombre = 0;
-        for (; indice < taille; indice++) {
-            nombre = 0;
-            for (int y = indicePuissance; y <= indice; y++) {
-                nombre += ((int)a[taille-y-1]-48) * pow(10, y-indicePuissance);
-            }  
-            if (nombre >= pow(2, 32) - 1) {
-                break;
-            }
-            
-        }
-        indicePuissance = indice;
-        nombreCase++;
-    }
-    return nombreCase;
-}
-void checkCharLength(char* nombre1, char* nombre2){
-    int nb1 = getNumbersChar(nombre1);
-    int nb2 = getNumbersChar(nombre2);
-    if(nb1 > nb2){
-        nombre2 = (char*)realloc(nombre2, nb1+1); // Add space for another character to be read.
-        int diff = nb1 - nb2;
-        for(int i = 0; i <= nb1; i++){
-            nombre2[nb1 - i] = nombre2[nb2-i];
-        }
-        for(int i=0; i<diff; i++){
-            nombre2[i] = '0';
-        }
-    }else if(nb2 > nb1){
-        nombre1 = (char*)realloc(nombre1, nb2+1); // Add space for another character to be read.
-        int diff = nb2 - nb1;
-        for(int i = 0; i <= nb2; i++){
-            nombre1[nb2 - i] = nombre1[nb1-i];
-        }
-        for(int i=0; i<diff; i++){
-            nombre1[i] = '0';
-        }
-    }
-}
-void remplirTableau(int taille, int tailleBase, char* a, unsigned int* nb) {
-    unsigned long int nombre = 0;
-    int indicePuissance = 0;
-    int indice = 0;
-    int caseRempli = 0;
-    for (int i = 0; i < tailleBase; i++) {
-        nombre = 0;
-        for (; indice < taille; indice++) {       
-            nombre = 0;
-            for (int y = indicePuissance; y <= indice; y++) {
-                nombre += ((int)a[taille-y-1] - 48) * pow(10, y - indicePuissance);
-            }
-            if (nombre >= pow(2, 32) - 1) {
-                indicePuissance = indice;
-                break;
-            }
-            nb[caseRempli] = nombre;
-        }
-        caseRempli++;
-    }
-}
-void dynamicChar(char*& nombre){
-    nombre = (char*)malloc(1);
-    int tmp;
-    int i = 0;
-    // Read characters until found an EOF or newline character. */
-    while((tmp = getchar()) != '\n' && tmp != EOF)
-    {
-        nombre[i++] = tmp;
-        nombre = (char*)realloc(nombre, i+1); // Add space for another character to be read.
-    }
-    nombre[i] = '\0';  // Null terminate the string
-}
 int main(int argc, const char * argv[]) {
     
     lentier a;
